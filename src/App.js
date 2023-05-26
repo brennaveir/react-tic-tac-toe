3918423e874b3ef4f8a3f5f7542e5c94eead6129
brennaveir <brennaveir@hotmail.com>
2023-05-26 13:41:36 -0400

    removed state from component square and gave it to the parent component board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Components/Square'
 
+
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
   return (
     <React.Fragment>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </React.Fragment>
   );
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,18 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
-function Square() {
-    const [value, setValue] = useState(null)
-    function handleClick() {
-        setValue('X')
-    }
+function Square({ value }) {
+
     return (
-        <button
-            className="square"
-            onClick={handleClick}
-        >
-            {value}
-        </button>
+        <button className="square">{value}</button>
     );
 }
 
