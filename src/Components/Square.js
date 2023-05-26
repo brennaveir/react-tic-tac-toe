a7faa7a27185d08e77334f32c3752119064a0c08
brennaveir <brennaveir@hotmail.com>
2023-05-26 13:35:37 -0400

    removed props and implemented useState instead to change square to an 'x' when clicked

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Square({ value }) {
-    return <button className="square">{value}</button>
 
+function Square() {
+    const [value, setValue] = useState(null)
+    function handleClick() {
+        setValue('X')
+    }
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
 }
 
 export default Square;
